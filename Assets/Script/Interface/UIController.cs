f0c3228e1d9c24b52c4229d57cd691aceaf7c782
DevNikz <paul_ragudo@dlsu.edu.ph>
2025-04-10 22:13:29 +0800

    Added ui stuffs

--- Assets/Script/Interface/UIController.cs
+++ Assets/Script/Interface/UIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class UIController : MonoBehaviour {
@@ -12,6 +13,26 @@ public class UIController : MonoBehaviour {
         Welcome = content.Find("WELCOME").gameObject;
         MainMenu = content.Find("MAIN MENU").gameObject;
         Loading = content.Find("LOADING").gameObject;
+        HideAllUI();
     }
 
+    
+    public void SetMainMenu() {
+        StartCoroutine(LoadingScreen());
+        HideAllUI();
+        MainMenu.SetActive(true);
+    }
+
+    void HideAllUI() {
+        Welcome.SetActive(false);
+        MainMenu.SetActive(false);
+        Loading.SetActive(false);
+    }
+
+
+    IEnumerator LoadingScreen() {
+        HideAllUI();
+        Loading.SetActive(true);
+        yield return new WaitForSeconds(2f);
+    }
 }
--- Assets/Script/Interface/UserController.cs
+++ Assets/Script/Interface/UserController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class UserController : MonoBehaviour {
     public static UserController instance;
-
+    UIController ui;
     public int currentState = -1; // -1 = Welcome | 0 = Main Menu
 
     void Awake() {
@@ -12,13 +12,23 @@ public class UserController : MonoBehaviour {
         else Destroy(this);
     }
 
-    public void Update()
+    void Start()
     {
-        if(Input.anyKeyDown) {
-            currentState = 0;
-        }  
+        ui = GameObject.FindGameObjectWithTag("HUD").GetComponent<UIController>();   
     }
 
+    public void Update()
+    {
+        switch(currentState) {
+            case -1:
+                if(Input.anyKeyDown) {
+                    currentState = 0;
+                    ui.SetMainMenu();
+                }  
+                break;
+        }
+        
+    }
 
     public int GetState() {
         return currentState;
